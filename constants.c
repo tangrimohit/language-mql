3ac7fc54b19a911a144e7fdefec6e0201a144d62
Abhisaran <30199540+Abhisaran@users.noreply.github.com>
2019-10-20 13:34:05 +0530

    day 1 github

---
+++ constants.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#define PI 3.14
+
+void main() {
+        /* my first program in C */
+        int radius;
+        float area;
+        printf("Please enter the radius of the circle:");
+        scanf("%d",&radius);
+        area = (radius * radius) * PI;
+        printf("%f sq\n",area);
+}
+//
+// Arithmetic *+-/   '%'    10%4 = 2  ++  --
+// Relational == != > < >= <=
+// Logical   && || !
+// Bitwise
+// Assignment lvalue=rvalue a%=b a = a%b  <<=  >>= &= |= ^=
+// Miscellaneous  ?:  sizeof(float)  & *
+//
+// a == b  Boolean True 1 False 0
+//
+//  !(b==2)
+// 000 >> 000
+// 001 >> 001
+// 010 >> 001
+// 011 >> 011
+//
+// p	q	p & q	p | q	p ^ q  ~P
+// 0	0	    0	    0	    0
+// 0	1	    0	    1     1
+// 1	1	    1	    1	    0
+// 1	0	    0	    1	    1
+//
+//
+// ?: expression? statement : statement;
+//
+//
+// ()
---
+++ desc_making.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+int main(int argc, char const *argv[]) {
+        int grade = "10";
+        switch (grade) {
+        case A: printf("%s\n", );
+
+                ;
+                break;
+        case b:;
+        case 10:;
+        case 1;
+        default:
+        }
+
+        return 0;
+}
