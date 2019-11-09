0e1a0c28c7aef72319a70d44ff547397297aa5c3
Abhisaran <30199540+Abhisaran@users.noreply.github.com>
2019-11-09 14:04:55 +0530

    Loops and desc making

--- desc_making.c
+++ desc_making.c
@@ -1,15 +1,36 @@
 #include "stdio.h"
 int main(int argc, char const *argv[]) {
-        int grade = "10";
-        switch (grade) {
-        case A: printf("%s\n", );
+        int grade = 10;
+        scanf("%d",&grade);
+        if(grade%10==0) {
+                printf("%d %d is an %d even number\n",grade,grade,grade);
+        }
+        else if(grade%3 == 0) {
+                printf("%d is a multiple of three\n",grade);
+        }
+        else if(grade %5 == 0) {
+                printf("%d is a multiple of five\n",grade);
+        }
+        else{
+                if(grade%7 == 0) {
+                        printf("%d is a multiple of seven\n",grade);
+                }
+                else{
+                        printf("%d is not even number\n",grade);
+                }
+        }
+        // nested if
 
-                ;
+        switch (4) {
+        case 0: printf("%d is an even number\n",grade);
                 break;
-        case b:;
-        case 10:;
-        case 1;
-        default:
+        case 1: printf("%d is an odd number\n",grade);
+                break;
+        case 'a': printf("There's A\n");
+                break;
+        default: printf("%d is not a valid number\n",grade);
+                break;
+        case 4: printf("4\n");
         }
 
         return 0;
