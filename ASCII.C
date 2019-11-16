659759fb3985d365f4ce0c5afdfaeb69795fade3
Abhisaran <30199540+Abhisaran@users.noreply.github.com>
2019-11-16 14:58:51 +0530

    sum

--- ASCII.C
+++ ASCII.C
@@ -2,12 +2,12 @@
 
 int main(int argc, char const *argv[]) {
         int i;
-        // for ( i = 0; i < 256; i++) {
-        //         /* code */
-        //         printf("ASCII of %c is %d\n",i,i);
-        // }
+        for ( i = 0; i < 256; i++) {
+                /* code */
+                printf("ASCII of %c is %d\n",i,i);
+        }
 
 
-        printf("This is to s%chow backspa%cce",127,8);
+        // printf("This is to s%chow backspa%cce",127,8);
         return 0;
 }
--- stringconvert.c
+++ stringconvert.c
@@ -5,11 +5,33 @@ int main(int argc, char const *argv[]) {
         int i=0;
         scanf("%s",str);
         for ( i = 0; i <strlen(str); i++) {
-                /* code */
-                if (int(str[i])) {
-                        /* code */
+                if ((int)str[i]>=65 && (int)str[i]<=90) {
+                        str[i] = (char)((int)str[i]+32);
+                }
+                else if ((int)str[i]>=97 && (int)str[i]<=122) {
+                        str[i] = (char)((int)str[i]-32);
+                }
+                else{
+                        printf("Please enter a string with only aplhabets");
+                        return 0;
                 }
         }
-
+        printf("Reversed string is %s",str);
         return 0;
 }
+
+struct  address {
+        int doorno;
+        char street[1000];
+}
+
+//
+// data_type variable[];
+//
+// int sum[100];
+//
+// print sum[99];
+//
+// int sum[] = {1,2,3,4,5}
+//
+// char string[4];
